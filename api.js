30ee075461fc5f64515be1f39f85277b5ff85376
Jhesimiel <jhesimieljorgedev@gmail.com>
2025-07-16 19:49:48 -0300

    feat: add button that switches a celsius and fahrenheit

--- api.js
+++ api.js
@@ -1,15 +1,19 @@
+const apiKey = "cd952c5e9a4cbf2d08008c6c0d510660";
 const cityName = document.querySelector("input");
-const searchButton = document.querySelector("button");
+const searchButton = document.querySelector(".search-button img");
 const weatherStatus = document.querySelector(".weather img");
 
-const apiKey = API_CONFIG.API_KEY;
-const baseUrl = API_CONFIG.BASE_URL;
-const defaultUnits = API_CONFIG.DEFAULT_UNITS;
 
 //TODO: Switcher °C and °F button
 async function consultWeather(city) {
   try {
-    cityName.value = "";
+    function ClearInput() {
+      cityName.value = "";
+      cityName.placeholder = "Type for a city...";
+      cityName.classList.remove("shake");
+      searchButton.classList.remove("shake");
+    }
+    ClearInput();
 
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`
@@ -20,14 +24,11 @@ async function consultWeather(city) {
     console.log(data);
     const { name, main, wind, weather } = data;
     const updateStatus = () => {
-      document.querySelector(".temp").innerText = `${Number(main.temp).toFixed(
-        0
-      )}°C`;
+
+      document.querySelector(".temp").innerText = `${Number(main.temp).toFixed(0)}°C`;
       document.querySelector(".city").innerText = name;
       document.querySelector(".humidity").innerText = `${main.humidity}%`;
-      document.querySelector(".wind").innerText = `${Number(wind.speed).toFixed(
-        1
-      )}km/h`;
+      document.querySelector(".wind").innerText = `${Number(wind.speed).toFixed(1)}km/h`;
 
       weather[0].main == "Clouds"
         ? (weatherStatus.src = "./assets/clouds.png")
@@ -46,12 +47,17 @@ async function consultWeather(city) {
     await updateStatus();
   } catch (error) {
     cityName.value = "";
-    cityName.classList.add("invalid");
+    cityName.classList.add("shake");
+    searchButton.classList.add("shake");
     cityName.placeholder = "Invalid City Name...";
   }
 }
-// TODO: API Search by pressing enter
+
+document.addEventListener("keydown", (event) => {
+  event.key === "Enter" ? consultWeather(`${cityName.value}`) : ""
+})
 
 searchButton.onclick = () => {
   consultWeather(`${cityName.value}`);
 };
+
